3d9461b98dd8f9f56375a1f7d7d230209e543960
Nadim Radjab <nadimradjab94@gmail.com>
2022-11-13 22:59:35 +0100

    Image compress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import "../i18next";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "../styles/Nprogress.css";
 import Navbar from "../components/Navbar";
-
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+NProgress.configure({ showSpinner: false });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
